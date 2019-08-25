b5cd807f03a7a697e7cd485b2750a93a0570c264
dpl10 <dlittle@nybg.org>
2019-08-25 15:21:09 -0400

    fix webpack output names

--- webpack-test.config.js
+++ webpack-test.config.js
@@ -4,27 +4,27 @@ const ROOT = path.resolve( __dirname, 'src' );
 const DESTINATION = path.resolve( __dirname, 'dist' );
 module.exports = {
 	context: ROOT,
+	devtool: 'cheap-module-source-map',
+	devServer: {},
+	module: {
+		rules: [
+			{
+				enforce: 'pre',
+				test: /\.js$/,
+				use: 'source-map-loader'
+			},
+			{
+				test: /\.ts$/,
+				exclude: [ /node_modules/ ],
+				use: 'awesome-typescript-loader'
+			}
+		]
+	},
 	resolve: {
 		extensions: ['.ts', '.js'],
 		modules: [
 				ROOT,
 				'node_modules'
 		]
-	},
-	module: {
-		rules: [
-				{
-					enforce: 'pre',
-					test: /\.js$/,
-					use: 'source-map-loader'
-				},
-				{
-					test: /\.ts$/,
-					exclude: [ /node_modules/ ],
-					use: 'awesome-typescript-loader'
-				}
-		]
-	},
-	devtool: 'cheap-module-source-map',
-	devServer: {}
+	}
 };
